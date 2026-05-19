faf79a1053290b8a9c85f7ca1f76315c52899ff3
ihack168 <ihack168@gmail.com>
2026-05-19 16:22:08 +0800

    s

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -1,8 +1,8 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "no0ub4vm", // 去 Sanity Manage 頁面找
+  projectId: "oo1n2doq", // 去 Sanity Manage 頁面找
   dataset: "production",      // 通常是 production
-  apiVersion: "2024-03-11",    // 使用當前的日期
+  apiVersion: "2024-5-11",    // 使用當前的日期
   useCdn: false,               // 開發或需要即時更新時設為 false
 });
