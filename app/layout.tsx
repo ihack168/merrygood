211aa8e164d2a0d1fc6a0ce36818385a1cd70544
ihack168 <ihack168@gmail.com>
2026-07-03 01:51:29 +0800

    sss

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Noto_Sans_TC, Geist_Mono } from "next/font/google"
 import Script from "next/script"
+import { Navbar } from '@/components/navbar'
 import "./globals.css"
 
 const notoSansTC = Noto_Sans_TC({
